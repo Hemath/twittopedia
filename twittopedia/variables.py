3f5fbc762d2e1751cf9079cf39cac6faebefadab
Hemath Kumar <43602569+Hemath@users.noreply.github.com>
2018-10-09 20:53:15 +0530

    Update variables.py

--- twittopedia/variables.py
+++ twittopedia/variables.py
@@ -6,14 +6,14 @@ Database_Name = "Twittopedia"
 
 # Tweepy Credentials
 
-Consumer_Key = "KADh512GrkiwikGbPYxkR4W3q"
+Consumer_Key = "Tour Consumer Key"
 
-Consumer_Key_Secret = "MkCeDYhGsnFO6so1e1HHD9yvPRE7qfarm0XzaIqM8OAEtD3o0l"
+Consumer_Key_Secret = "Ypur consumer key secret"
 
-Access_Token = "914389946340986882-6uusSsm4ePbtd7kLnRBSOgwNwwaoKCx"
+Access_Token = "Your Access Token"
 
-Access_Token_Secret = "5UQO7OGEFtLgcmHLrwKNoSKIREFXS030r57eXJKfUycVJ"
+Access_Token_Secret = "Access Token Secret"
 
 # App Variables
 
-Secret_Key = "0BTY3QKJ3H8MGLNH"
+Secret_Key = "Secret Key For Flask Sessions"
